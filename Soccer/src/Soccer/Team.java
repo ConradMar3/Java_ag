e6d692c17ece53fa15f26b429f4612842eed4937
ConradMar3 <96224782+ConradMar3@users.noreply.github.com>
2022-03-06 15:26:36 +0200

    2022/03/06 (pm)
    Chapter09_ag : Encapsulation.

---
+++ Soccer/src/Soccer/Team.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Soccer;
+
+/**
+ *
+ * @author maria
+ */
+public class Team {
+    
+    private String teamName;
+    private Player[] playerArray;
+
+    /**
+     * @return the teamName
+     */
+    public String getTeamName() {
+        return teamName;
+    }
+
+    /**
+     * @param teamName the teamName to set
+     */
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    /**
+     * @return the playerArray
+     */
+    public Player[] getPlayerArray() {
+        return playerArray;
+    }
+
+    /**
+     * @param playerArray the playerArray to set
+     */
+    public void setPlayerArray(Player[] playerArray) {
+        this.playerArray = playerArray;
+    }
+    
+}
+
